5841ef69c1e08850f9ae1615bf9daff9388a5e3c
GoToSchoolTeams <schoolgoto@gmail.com>
2023-09-04 11:52:13 +0900

    fix

--- 2.SecondSemester/co3_ExceptionAndAsync/temp_HttpResource/HelloGetHttp.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*Getデータの受け取り*/
-$getName = $_GET['name'];
-$getScore = $_GET['score'];
-
-/*出力 */
-print("HelloGetHttp!:");
-print($getName);
-print($getScore);
--- 2.SecondSemester/co3_ExceptionAndAsync/temp_HttpResource/HelloPostHttp.php
+++
@@ -1,9 +0,0 @@
-<?php
-/*Getデータの受け取り*/
-$postName = $_POST['name'];
-$postScore = $_POST['score'];
-
-/*出力 */
-print("HelloPostHttp!:");
-print($postName);
-print($postScore);
